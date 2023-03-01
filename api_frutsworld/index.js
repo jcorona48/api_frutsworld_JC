888ad1fc20616bae4249d62a4e97f2616cbff520
jcorona48 <105407871+jcorona48@users.noreply.github.com>
2023-03-01 04:36:00 -0400

    Habilite import y las querys
    Descargue las nuevas dependencias sql2 y lo hice funcionar junto a la base de datos.

--- api_frutsworld/index.js
+++ api_frutsworld/index.js
@@ -1,19 +1,50 @@
-const keys = require("./keys");
 
+import {keys} from "./keys.js"
 // Express App Setup
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mysql from 'mysql'
+import {pool} from "./db.js"
+
+let con = mysql.createConnection({
+    host: keys.msqlHost,
+    user: keys.msqlUser,
+    password: keys.msqlPassword,
+    port: keys.msqlPort,
+    database: keys.msqlDatabase,
+});
+
+con.connect(function(err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+    console.log('Connected to the MySQL server.');
+});
 
 const app = express();
 
-console.log("SIIIU");
+app.get("/Productos/all", async (req, res) => {
+  const [result] = await pool.query("SELECT 1 + 1 as result");
+  res.json(result[0]);
+});
 
 app.listen(5000, (err) => {
   console.log("Listening");
 });
 
+app.get("/Productos", (req, res) => {res.send("Obteniendo Productos"); });
+
+app.post("/Productos", (req, res) => {res.send("Creando Empleados"); });
+
+app.put("/Productos", (req, res) => {res.send("Actualizando Productos"); });
+
+app.delete("/Productos", (req, res) => {res.send("Eliminando Productos"); });
+
+console.log("klk");
+
 /*
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -64,5 +95,6 @@ app.post("/authors", async (req, res) => {
 
     res.send({ working: true });
 });
-*/
 
+
+*/
--- api_frutsworld/keys.js
+++ api_frutsworld/keys.js
@@ -1,7 +1,9 @@
-module.exports = {
+
+export const keys = {
   msqlUser: process.env.MSQLUSER,
   msqlHost: process.env.MSQLHOST,
   msqlDatabase: process.env.MSQLDATABASE,
   msqlPassword: process.env.MSQLPASSWORD,
   msqlPort: process.env.MSQLPORT,
-};
+}
+
